a1af6cfc08e9656c1678a9eedc85a4cdeb5e643c
JESÚS GONZÁLEZ MORENO <Jesus.Gonzalez27@alu.uclm.es>
2022-09-30 20:31:12 +0200

    test agregarJugador y unirseAPartida

--- cliente/spec/modeloSpec.js
+++ cliente/spec/modeloSpec.js
@@ -17,18 +17,26 @@ describe("El juego...", function() {
     expect(urs2.nick).toEqual("luis");
   });
 
-  xit("crear partida", function() {
+  it("crear partida", function() {
     let codigo=usr1.crearPartida();
     expect(miJuego.partidas[codigo]).toBeDefined();
     let partida=miJuego.partidas[codigo];
     expect(partida.owner,nick).toEqual(usr1.nick);
     expect(partida.jugadores[0].nick).toEqual(usr1.nick);
-    expect(partidas.codigo).toEqual(codigo);
+    expect(partida.codigo).toEqual(codigo); //antes ponia partidas, le he borrado la "s"
   });
 
-  xit("el usuario luis se une a la partida",function(){
+  it("el usuario luis se une a la partida", function(){
+    let codigo=usr1.crearPartida();
+    let partida=miJuego.partidas[codigo];
+    usr2.unirseAPartida();
+    expect(partida.jugadores.length).toEqual(2);
+    expect(partida.jugadores[1].nick).toEqual(usr2.nick);
+    
 
     
   });
+
+
 
 });
