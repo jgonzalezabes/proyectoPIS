0826a7ef2e3eeabbf5df541ef2ffd86e25ae9562
JESÚS GONZÁLEZ MORENO <jgonzalezabes@gmail.com>
2023-01-20 13:40:51 +0100

    URL del passport cambiada

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -15,7 +15,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID:"571253589268-nn1s2cmb9e0cclp0v4r5pvtfllkvkgpc.apps.googleusercontent.com",
     clientSecret:"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",
-    callbackURL:"http://localhost:3000/google/callback"
+    callbackURL:"https://proyecto22-3moug36toq-tl.a.run.app/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     //User.findOrCreate({ googleId: profile.id }, function (err, user) {
